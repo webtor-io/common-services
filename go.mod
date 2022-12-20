925e7a92d0fd55366487501496e70b1691c91d85
Pavel Tatarskiy <pavel.tatarsky@gmail.com>
2022-12-20 21:23:22 +0300

    add prometheus endpoint service

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/webtor-io/common-services
 go 1.15
 
 require (
-	github.com/aws/aws-sdk-go v1.36.28
+	github.com/aws/aws-sdk-go v1.44.163
 	github.com/cpuguy83/go-md2man/v2 v2.0.0 // indirect
 	github.com/go-pg/migrations/v8 v8.1.0
 	github.com/go-pg/pg/v10 v10.9.1
@@ -11,6 +11,7 @@ require (
 	github.com/onsi/ginkgo v1.14.2 // indirect
 	github.com/onsi/gomega v1.10.4 // indirect
 	github.com/pkg/errors v0.9.1
+	github.com/prometheus/client_golang v1.14.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/sirupsen/logrus v1.7.0
 	github.com/urfave/cli v1.22.5
---
+++ prom.go
@@ -0,0 +1,63 @@
+package services
+
+import (
+	"fmt"
+	"net"
+	"net/http"
+
+	"github.com/pkg/errors"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/sirupsen/logrus"
+	"github.com/urfave/cli"
+)
+
+const (
+	promHostFlag = "prom-host"
+	promHostPort = "prom-port"
+)
+
+type Prom struct {
+	host string
+	port int
+	ln   net.Listener
+}
+
+func NewProm(c *cli.Context) *Prom {
+	return &Prom{
+		host: c.String(promHostFlag),
+		port: c.Int(promHostPort),
+	}
+}
+
+func RegisterPromFlags(f []cli.Flag) []cli.Flag {
+	return append(f,
+		cli.StringFlag{
+			Name:  promHostFlag,
+			Usage: "prometheus metrics listening host",
+			Value: "",
+		},
+		cli.IntFlag{
+			Name:  promHostPort,
+			Usage: "prometheus metrics listening port",
+			Value: 8082,
+		})
+}
+
+func (s *Prom) Serve() error {
+	addr := fmt.Sprintf("%s:%d", s.host, s.port)
+	ln, err := net.Listen("tcp", addr)
+	if err != nil {
+		return errors.Wrap(err, "Failed to web listen to tcp connection")
+	}
+	s.ln = ln
+	mux := http.NewServeMux()
+	mux.Handle("/metrics", promhttp.Handler())
+	logrus.Infof("Serving Prom Metrics at %v", addr)
+	return http.Serve(ln, mux)
+}
+
+func (s *Prom) Close() {
+	if s.ln != nil {
+		s.ln.Close()
+	}
+}
